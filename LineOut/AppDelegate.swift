0d8d3c65f6b0c0ef8c761dfb5096fa0c97a2bf7e
Peter Wunder <hallo@peterwunder.de>
2019-06-17 15:18:16 +0200

    Initial Commit

---
+++ LineOut/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LineOut
+//
+//  Created by Peter Wunder on 17.06.19.
+//  Copyright © 2019 Peter Wunder. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ LineOut/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LineOut
+//
+//  Created by Peter Wunder on 17.06.19.
+//  Copyright © 2019 Peter Wunder. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
